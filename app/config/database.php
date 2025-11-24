1155942c9d0aba3c551f2b4e385bcfd691d1c5e1
alaizha <ylaganannalyzha@gmail.com>
2025-11-24 14:02:16 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
    $database['main'] = array(
         'driver'	=> 'mysql',
-        'hostname'	=> 'localhost',
+        'hostname'	=> 'sql12.freesqldatabase.com',
         'port'		=> '3306',
-        'username'	=> 'root',
-        'password'	=> '',
-        'database'	=> 'mockdata',
+        'username'	=> 'sql12809110',
+        'password'	=> '8yECb1f9WE',
+        'database'	=> 'sql12809110',
         'charset'	=> 'utf8mb4',
         'dbprefix'	=> '', // Optional for SQLite
         'path'      => ''
